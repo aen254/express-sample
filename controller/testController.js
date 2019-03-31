7a914316aff1d54dc288cd5030ed83fbe89b64a3
pornpan <aen254@gmail.com>
2019-03-31 10:37:35 +0700

    update

--- controller/testController.js
+++ controller/testController.js
@@ -8,21 +8,51 @@ let responseObject = {
     result: ""
 }
 
+// req.qury.key1
 router.get("/getData",  (req, res) => {
-    let result = req.query.key1 * req.query.key2;
-    res.send({ result: result })
+    let result = Number(req.query.key1) + Number(req.query.key2);
+    res.send({ sum : result })
 });
+//base =10
+//height = 20
+router.get("/getTriangleArea", (req, res) => {
+    let result = (1/2)*req.query.base*req.query.height
+    res.send({ TriangleArea : result })
+});
+
+//weight
+//height
+router.get("/getBMI", (req, res) => {
+    let result = (req.query.weight/((req.query.height/100)*(req.query.height/100))).toFixed(2);
+    let msg = "";
+    if(result>25){
+        msg = "Fat";
+    }else if(result==25){
+        msg = "Normal";
+    }else if(result<25){
+        msg = "Thin"
+    }
+
+    res.send({ BMI : result, Msg : msg })
+});
+
 /*
 Request
+key
 * */
+
 router.post("/postData",async  (req, res) => {
-    if(!req.body.key5){
+    responseObject = {
+        msg: "",
+        result: ""
+    }
+    if(!req.body.key){
         responseObject.msg = "Bad request";
     }else{
         responseObject.msg = "success";
         responseObject.result = req.body.key * 5;
     }
-    res.send(responseObject);
+     res.send(responseObject);
 });
 
 module.exports = router;
